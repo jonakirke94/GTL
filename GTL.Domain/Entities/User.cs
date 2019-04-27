1bf1fb8fd464a358d0d75c35c72306e87743113b
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-04-27 19:49:23 +0200

    removed unused references

--- GTL.Domain/Entities/User.cs
+++ GTL.Domain/Entities/User.cs
@@ -1,11 +1,5 @@
-﻿using GTL.Domain.Entities.Identity;
-using GTL.Domain.Enums;
-using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GTL.Domain.Entities
 {
