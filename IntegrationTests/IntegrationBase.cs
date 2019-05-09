b725f8a22b034d233d54e95933a35bf16ee84ac4
Jonathan <jonakirke94@gmail.com>
2019-05-09 13:27:50 +0200

    added example of integration test with delete / seed sql script running before and after each test

---
+++ IntegrationTests/IntegrationBase.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using Dapper;
+using GTL.Application.Interfaces.UnitOfWork;
+using GTL.Web;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace IntegrationTests
+{
+    public class IntegrationBase : IDisposable
+    {
+        private readonly IGTLContext _context;
+
+        protected IMediator _mediator;
+
+        public IntegrationBase()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .ConfigureAppConfiguration(config =>
+                    config.AddJsonFile("appsettings.test.json", optional: true, reloadOnChange: true))
+                .UseStartup<Startup>());
+
+            _mediator = server.Host.Services.GetRequiredService<IMediator>();
+
+            _context = server.Host.Services.GetRequiredService<IGTLContext>();
+
+            ResetDatabase();
+        }
+
+        public void Dispose()
+        {
+            ResetDatabase();
+        }
+
+        public void ResetDatabase()
+        {
+            RunScript("ResetDatabase");        
+        }
+
+        public void SeedDatabase()
+        {
+            RunScript("SeedDatabase");
+        }
+
+        public void RunScript(string fileName)
+        {
+            var script = File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "/" + fileName + ".sql");
+
+            using (var cmd = _context.CreateCommand())
+            {
+                foreach (var sqlBatch in script.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    cmd.Connection.Execute(sqlBatch);
+                }
+            }
+        }
+    }
+}
---
+++ IntegrationTests/MediatorIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GTL.Application.Interfaces.Repositories;
+using GTL.Application.UseCases.Login.Commands;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class MediatorIntegrationTests : IntegrationBase
+    {
+        public MediatorIntegrationTests()
+        {
+            SeedDatabase();
+        }
+
+        [Fact(DisplayName = "Test Login handler")]
+        public async Task TD2_TC2()
+        {
+            // Arrange
+            var command = new LoginCommand
+            {
+                Email = "thanos@gmail.com",
+                Password = "12345678"
+            };
+
+            var response = await _mediator.Send(command);
+
+
+            Assert.False(response);
+
+            //var mediator = testServer.Host.Services.GetRequiredService<IMediator>();
+
+
+            // Assert
+        }
+
+    }
+}
