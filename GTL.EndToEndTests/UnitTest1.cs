fc2ea38c45a47e8026bc43874e7c6a1fa583d4b1
Stefan Krabbe <stefankrabbe54@gmail.com>
2019-05-10 20:09:52 +0200

    Made two example projects

---
+++ GTL.EndToEndTests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace GTL.EndToEndTests
+{
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        private string GetDriverPath()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Fact]
+        public void CanOpenGoogleTest()
+        {
+            driver = new ChromeDriver(GetDriverPath());
+
+            driver.Navigate().GoToUrl("http://google.com/");
+
+            Console.WriteLine("Test completed");
+
+            driver.Close();
+        }
+
+        [Fact]
+        public void TheSampleTest()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44327/");
+            driver.FindElement(By.LinkText("GTL")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+    }
+}
